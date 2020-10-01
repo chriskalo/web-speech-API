07113a455c88d8e9fac076858e0c19498fb69c39
Christodoulos <43905544+chriskalo@users.noreply.github.com>
2020-10-01 23:27:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
+var hasSpoken = false;
 
 var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
@@ -8,7 +9,9 @@ recognition.onstart = function () {
   console.log("voice is activated, you can speak");
 };
 
+
 recognition.onresult = function (event) {
+  hasSpoken = true;
   const current = event.resultIndex;
   const transcript = event.results[current][0].transcript;
   clearBox("sound");
@@ -16,12 +19,38 @@ recognition.onresult = function (event) {
 };
 
 btn.addEventListener ('click', () => {
+  clearBox("sound");
   clearBox("cont");
+  playclip();
   recognition.start();
   document.getElementById("sound").innerHTML +=  '<img src="sound.gif" style="width:250px;height:250p;">';
+});
+
+recognition.addEventListener('end', function() {
+  playclip();
+  if (hasSpoken == false) {
+    clearBox("sound");
+  }
+  hasSpoken = false;
 });
 
 function clearBox(elementID)
 {
     document.getElementById(elementID).innerHTML = "";
+}
+
+function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "soundEffect.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByTagName("audio")[0];
+audio.play();
+}
+}
 }
